6ef1c492b5199ef05042eb9a7107c253f4151075
Biniyam <160533459+BiniyamTG@users.noreply.github.com>
2024-11-04 15:10:55 +0300

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,6 +9,51 @@ const Projects = () => {
   return (
     <section id="project" className="max-w-container mx-auto lgl:px-20 py-24">
       <SectionTitle title="My Projects" titleNo="03" />
+      {/* ============ project Two Start here ================ */}
+        <div className="flex flex-col xl:flex-row-reverse gap-6">
+          <a
+            className="w-full xl:w-1/2 h-auto relative group"
+              href="https://biniyamtg.github.io/CoupleCard/"
+            target="_blank"
+          >
+            <div>
+              <Image
+                className="w-full h-full object-contain"
+                src={cyberImg}
+                alt="cyberImg"
+              />
+              <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
+            </div>
+          </a>
+          <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-between items-end text-right z-10">
+            <div>
+              <p className="font-titleFont text-textGreen text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">እንጠያየቅ?</h3>
+            </div>
+            <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md xl:-mr-16">
+            Is designed to foster <span className="text-textGreen">deeper conversations between couples.</span> It provides a collection of thought-provoking prompts and questions that encourage meaningful discussions and strengthen emotional connections. {" "}
+        
+            </p>
+            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+              <li>js</li>
+        
+            </ul>
+            <div className="text-2xl flex gap-4">
+              
+      
+              <a
+                className="hover:text-textGreen duration-300"
+                href="https://biniyamtg.github.io/CoupleCard/"
+                target="_blank"
+              >
+                <RxOpenInNewWindow />
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* ============ project Two End here ================== */}
       {/* ============ project One Start here ================ */}
       <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
         <div className="flex flex-col xl:flex-row gap-6">
